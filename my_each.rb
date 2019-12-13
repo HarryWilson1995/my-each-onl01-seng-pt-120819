3c752f71f61d2bbfabef9083b87a0757d8c6f3de
Harry Wilson <harry.wilson1995a@gmail.com>
2019-12-13 19:51:23 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,24 +1,2 @@
-array = [1, 2, 3, 4]
-
-# def my_each(array)
-#   i = 0 
-#   while i < array.length 
-#   yield (array[i])
-#   i = i + 1 
-# end 
-# end
-
-# my_each(array) {|i| puts i} 
-
-def hello(array)
-  i = 0
-  collection = []
-  while i < array.length
-    collection << yield(array[i])
-    i += 1
-  end
-  collection
-end
-
-hello(array) {|i| puts i}
+def my_each()
 
