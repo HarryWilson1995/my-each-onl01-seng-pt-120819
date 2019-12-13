f621950436df9c8d7fd2e9f918214443ac6c8498
Harry Wilson <harry.wilson1995a@gmail.com>
2019-12-13 19:57:57 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,4 +1,4 @@
-list = ["ab", "cd", "ef"]
+# list = ["ab", "cd", "ef"]
 
 def my_each(array)
   i = 0
@@ -7,6 +7,7 @@ def my_each(array)
     yield(array[i])
     i = i + 1
   end
+  array 
 end
 
 my_each(list) {|i| i}
